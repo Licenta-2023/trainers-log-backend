5f563ea5e272bcacf3f2bb857674471604c4c56a
Daci1 <dacian.barbu28@gmail.com>
2022-11-15 16:46:05 +0200

    TLB-1: creates the base project with the required dependencies, uses .env for the database credentials

---
+++ README.md
@@ -0,0 +1,8 @@
+# Trainers Log Backend
+
+## .env file
+```
+DATABASE_URL=<url>
+DATABASE_USERNAME=<username>
+DATABASE_PASSWORD=<password>
+```
---
+++ src/main/java/com/trainerslog/backend/TrainersLogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trainerslog.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainersLogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainersLogBackendApplication.class, args);
+	}
+
+}
